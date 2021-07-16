cc3707bf67e553bf82568381fc8bee5de2813b86
lorossi <contact@lorenzoros.si>
2021-07-16 11:17:54 +0200

    code cleaned

--- js/sketch.js
+++ js/sketch.js
@@ -1,21 +1,15 @@
-const TWO_PI = Math.PI * 2;
-const PI = Math.PI;
-
 class Sketch extends Engine {
   preload() {
     // parameters
-    this._duration = 900;
-    this._recording = false;
-    this._show_fps = false;
-    this._lines_spacing = 20;
-    this._border = 0;
+    this._duration = 600;
+    this._recording = true;
+    this._lines_spacing = 15;
     this._scl = 1; // pixel scaling in final image
-    this._temp_canvas_size = 200;
-
-    this._loadTextPixels();
   }
 
   setup() {
+    // load pixels from text
+    this._loadTextPixels();
     // sketch setup
     console.clear();
     // setup capturer
@@ -33,13 +27,11 @@ class Sketch extends Engine {
     }
 
     const percent = (this._frameCount % this._duration) / this._duration;
-    //const percent = 0.5;
-    const time_theta = ease(percent) * PI;
+    const time_theta = ease(percent) * Math.PI;
 
     // DRAW
     const channel = 220 + 20 * Math.sin(time_theta); // RGB channel value
-    const alpha = 0.8 + 0.2 * Math.sin(time_theta); // RGB alpha
-    const dy = this._height * this._border / 2; // height displacement
+    const alpha = 0.7 + 0.2 * Math.sin(time_theta); // RGB alpha
 
     this._ctx.save();
     this.background("rgb(15, 15, 15)");
@@ -47,8 +39,8 @@ class Sketch extends Engine {
     this._ctx.lineWidth = 2;
 
 
-    for (let y = dy + this._lines_spacing / 2; y < this._height - dy; y += this._lines_spacing) {
-      // is the line in the old canvas aswell?
+    for (let y = this._lines_spacing / 2; y < this._height; y += this._lines_spacing) {
+      // is the line in the old canvas as well?
       const line_picked = this._pixels.filter(p => Math.abs(p.y - y) < this._temp_canvas_ratio);
       this._ctx.save();
       this._ctx.translate(0, y);
@@ -58,20 +50,20 @@ class Sketch extends Engine {
       for (let x = 0; x <= this._width; x += this._scl) {
         const height_percent = y / this._height; // height ratio
         const width_percent = x / this._width; // width ratio
-        const omega = TWO_PI * 20; // sin omega
-        const phi = height_percent * TWO_PI * 4 + percent * TWO_PI * 16; // sin phase
+        const omega = Math.PI * 2 * 20; // sin omega
+        const phi = height_percent * Math.PI * 2 * 4 + percent * Math.PI * 2 * 16; // sin phase
 
-        let ampl = this._lines_spacing * Math.sin(width_percent * omega + phi) * 0.35; // sin amplitude
+        let module = this._lines_spacing * Math.sin(width_percent * omega + phi) * 0.35; // sin amplitude
         // is this col picked? since the line is already picked, the pixel is picked
         const col_picked = line_picked.some(p => Math.abs(p.x - x) < this._temp_canvas_ratio);
         if (col_picked) {
           // add some noise if the letter is behind this pixel
-          ampl += Math.sin(time_theta) * random(-1, 1) * this._lines_spacing * 0.5;
-          // lower the risolution
+          module += Math.sin(time_theta) * random(-1, 1) * this._lines_spacing * 0.5;
+          // lower the resolution
           x += this._scl * 2;
         }
 
-        this._ctx.lineTo(x, ampl);
+        this._ctx.lineTo(x, module);
       }
       this._ctx.stroke();
       this._ctx.restore();
@@ -90,23 +82,15 @@ class Sketch extends Engine {
         console.log("%c Recording ended", "color: red; font-size: 2rem");
       }
     }
-
-    // show FPS
-    if (this._show_fps) {
-      this._ctx.save();
-      this._ctx.fillStyle = "red";
-      this._ctx.font = "30px Hack";
-      this._ctx.fillText(parseInt(this._frameRate), 40, 40);
-      this._ctx.restore();
-    }
   }
 
   _loadTextPixels() {
+    const temp_canvas_size = 200;
     // temp canvas parameters
-    const height = this._temp_canvas_size;
-    const width = this._temp_canvas_size;
+    const height = temp_canvas_size;
+    const width = temp_canvas_size;
     const border = 0.1 * height;
-    this._temp_canvas_ratio = this._height / this._temp_canvas_size; // ratio between temp canvas and real canvas
+    this._temp_canvas_ratio = this._height / temp_canvas_size; // ratio between temp canvas and real canvas
     // create temp canvas
     let temp_canvas;
     temp_canvas = document.createElement("canvas");
@@ -155,20 +139,8 @@ const xy_from_index = (i, width, ratio = 1) => {
   return { x: x * ratio, y: y * ratio };
 };
 
-const index_from_xy = (x, y, width) => {
-  return x + width * y;
-};
-
 const random = (a, b) => {
   if (a == undefined && b == undefined) return random(0, 1);
   else if (b == undefined) return random(0, a);
   else if (a != undefined && b != undefined) return Math.random() * (b - a) + a;
-};
-
-const distSq = (x1, y1, x2, y2) => {
-  return Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2);
-};
-
-const dist = (x1, y1, x2, y2) => {
-  return Math.sqrt(distSq(x1, y1, x2, y2));
 };
